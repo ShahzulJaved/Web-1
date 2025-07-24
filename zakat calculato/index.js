19b8efcc0e25f1355c2935e4fbc3445d3f7e7fa9
Shahzul Javed <shahzul.dev@gmail.com>
2025-07-24 19:04:24 +0500

    Add files via upload

---
+++ zakat calculato/index.js
@@ -0,0 +1,10 @@
+total_worth=prompt("Enter your wort")
+zakat_ratio=prompt("Enter zakat ratio");
+
+amount_of_zakat=total_worth*2.5/100
+
+alert(`your amount of zakat is Rs ${amount_of_zakat}`)
+document.write(`your amount of zakat is Rs ${amount_of_zakat}`)
+
+
+console.log( `your amount of zakat is Rs ${amount_of_zakat}`) 
